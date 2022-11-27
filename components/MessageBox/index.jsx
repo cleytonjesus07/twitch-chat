1a80e3df585d00fc40bcb9af3fe5ee5a8853d9d3
Cleyton Jesus <cleytonjesus007@gmail.com>
2022-11-26 23:40:46 -0300

    first commit

--- components/MessageBox/index.jsx
+++ components/MessageBox/index.jsx
@@ -1,14 +1,14 @@
 import { useRef } from "react"
 
 export default function messageBox({  chosen, setMessage, setShowInfoType }) {
-    const inputRef = useRef();
+    const InputRef = useRef();
     return (
         <form className="form-chat" id="form-chat">
-            <input ref={inputRef} className="chat-input" id="inputMessage" placeholder="Envie uma mensagem... :D"
+            <input ref={InputRef} className="chat-input" id="inputMessage" placeholder="Envie uma mensagem... :D"
                 onChange={(e) => (e.target.value.length > 0) ? setShowInfoType(true) : setShowInfoType(false)}
                 onBlur={() => setShowInfoType(false)}
             />
-            <button type="button" onClick={() => send(inputRef, setMessage, chosen)}>Enviar</button>
+            <button type="button" onClick={() => send(InputRef, setMessage, chosen)}>Enviar</button>
         </form>
     )
 }
